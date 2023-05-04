a9d0f8ae72a2c7bc708538f55895bd2952203a56
exam <exam@Bcalab4-m11>
2023-05-04 09:56:08 +0530

    first comit

---
+++ CaseToolsExam/src/Book_212.java
@@ -0,0 +1,16 @@
+
+public class Book_212 {
+int page_num;
+int total_page;
+
+Book_212(int total_pages)
+{
+	this.total_pages=total_pages;
+	this.page_num=1;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
